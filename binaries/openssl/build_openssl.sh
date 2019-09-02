e77edc52dd98fbe470eb9b5a5ffe39d9bd7f732b
keithrozario <keithjosephrozario@gmail.com>
2019-09-02 20:36:24 +0800

    added references to tfvar.json file

--- binaries/openssl/build_openssl.sh
+++ binaries/openssl/build_openssl.sh
@@ -3,18 +3,32 @@
 set -e
 VERSION=1.1.1c
 LICENSE=Apache-2.0
+ARTIFACT=openssl.zip
+
+STAGE=$1
+
+TFVAR_BUCKET=.s3bucket_layers[\""$STAGE"\"]
+BUCKET_NAME=$(cat ../../infra/terraform.tfvars.json | jq -r "$TFVAR_BUCKET")
+
+TFVAR_REGION=.aws_region[\""$STAGE"\"]
+AWS_REGION=$(cat ../../infra/terraform.tfvars.json | jq -r "$TFVAR_REGION")
+
+echo "Uploading to Bucket: $BUCKET_NAME in $AWS_REGION"
 
 rm -rf layer
 docker build --build-arg version=$VERSION -t keithrozario/openssl .
 CONTAINER=$(docker run -d keithrozario/openssl:latest false)
-docker cp $CONTAINER:/tmp/layer/openssl.zip openssl.zip
+docker cp $CONTAINER:/tmp/layer/openssl.zip $ARTIFACT
 docker rm $CONTAINER
 
 ZIPHASH=$(openssl dgst -sha256 openssl.zip | cut -d " " -f 2)
 
-# aws s3 cp openssl.zip s3://$1 --profile KlayersAccount
-aws s3api put-object-tagging --bucket $1 --key openssl.zip \
+aws s3 cp $ARTIFACT s3://$BUCKET_NAME --profile KlayersAccount --region $AWS_REGION
+aws s3api put-object-tagging --bucket $BUCKET_NAME --key $ARTIFACT \
 --tagging "TagSet=[{Key=Version,Value=$VERSION}, \
-python{Key=SHA256, Value=$ZIPHASH}, \
-{Key=License, Value=$LICENSE} ]" --profile KlayersAccount
-aws s3api get-object-tagging --bucket $1 --key openssl.zip --profile KlayersAccount
+{Key=SHA256, Value=$ZIPHASH}, \
+{Key=License, Value=$LICENSE} ]" --profile KlayersAccount --region $AWS_REGION
+ aws s3api get-object-tagging --bucket $BUCKET_NAME --key $ARTIFACT --profile KlayersAccount --region $AWS_REGION
+
+# Invoke deploy_binary lambda with package=openssl and package_artifact=openssl.zip
+# sls invoke --stage <stage> 
