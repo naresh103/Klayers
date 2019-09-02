2191345a6cffdcb7e66ae19ebb91e3ce64917569
keithrozario <keithjosephrozario@gmail.com>
2019-09-02 20:11:10 +0800

    added tags to S3 upload

--- binaries/openssl/build_openssl.sh
+++ binaries/openssl/build_openssl.sh
@@ -1,11 +1,20 @@
 #!/usr/bin/env bash
 
 set -e
+VERSION=1.1.1c
+LICENSE=Apache-2.0
 
 rm -rf layer
-docker build -t keithrozario/openssl .
+docker build --build-arg version=$VERSION -t keithrozario/openssl .
 CONTAINER=$(docker run -d keithrozario/openssl:latest false)
 docker cp $CONTAINER:/tmp/layer/openssl.zip openssl.zip
 docker rm $CONTAINER
 
-# openssl dgst -sha256 openssl.zip
+ZIPHASH=$(openssl dgst -sha256 openssl.zip | cut -d " " -f 2)
+
+# aws s3 cp openssl.zip s3://$1 --profile KlayersAccount
+aws s3api put-object-tagging --bucket $1 --key openssl.zip \
+--tagging "TagSet=[{Key=Version,Value=$VERSION}, \
+python{Key=SHA256, Value=$ZIPHASH}, \
+{Key=License, Value=$LICENSE} ]" --profile KlayersAccount
+aws s3api get-object-tagging --bucket $1 --key openssl.zip --profile KlayersAccount
